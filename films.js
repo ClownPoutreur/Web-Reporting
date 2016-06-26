1e07226c1f1bfd9641ad037ae621602f5a1f6da2
ClownPoutreur <musicforlifeandbeyond@gmail.com>
2016-06-26 21:58:58 +0200

    ajout de stats geo sur des films

---
+++ films.js
@@ -0,0 +1,21 @@
+	google.charts.load('current', {'packages': ['geochart']});
+    google.charts.setOnLoadCallback(drawMarkersMap);
+      
+	function drawMarkersMap() {
+		
+      var data = $.ajax({
+          url: "machinesData.json", //Change this for the url where is allocated the json or php file
+          dataType:"json",
+          async: false
+          }).responseText;
+
+      jsonData = JSON.parse(data);
+	  
+      var options = {
+		title : 'Repartition des films par nationalité dans une filmotheque personnelle',
+      };
+
+      var geochart = new google.visualization.GeoChart(document.getElementById('geochart'));
+      chart.draw(data, options);  
+	
+	} 
